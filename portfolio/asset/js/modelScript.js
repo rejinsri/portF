46b370a90dc8f32abf709b1d8e2f1e97c2aa392c
rejinsri <reji0510@gmail.com>
2024-02-15 22:37:42 +0530

    first commit

---
+++ portfolio/asset/js/modelScript.js
@@ -0,0 +1,10 @@
+ // JavaScript functions to show and hide the modal
+ function showSkillsModal() {
+    document.getElementById('modalOverlay').style.display = 'block';
+    document.getElementById('skills_set').style.display = 'block';
+}
+
+function hideSkillsModal() {
+    document.getElementById('modalOverlay').style.display = 'none';
+    document.getElementById('skills_set').style.display = 'none';
+}
